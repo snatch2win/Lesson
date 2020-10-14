c45710a820a2cfabf274f5ed2f623701d1377849
ilya <opt.tzk@gmail.com>
2020-10-14 22:59:38 +0300

    Initial commit

---
+++ src/Chapter_1/task_001.java
@@ -0,0 +1,23 @@
+//Вывести на одной строке числа 31, 18 и 79 с одним пробелом между ними. Текст '31 18 79' не использовать.
+
+
+package Chapter_1;
+
+public class task_001 {
+
+    public static void main(String[] args) {
+        int a = 31;
+        int b = 18;
+        int c = 79;
+
+        System.out.print(a + " ");
+        System.out.print(b + " ");
+        System.out.print(c);
+    }
+
+
+
+
+
+
+}
