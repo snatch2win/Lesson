e155b63b03dbd55b590ba4192b76d01e253b3c41
ilya <opt.tzk@gmail.com>
2020-10-15 00:07:16 +0300

    task5

--- src/Chapter_1/task_004.java
+++ src/Chapter_1/task_004.java
@@ -1,4 +1,13 @@
+//Вывести на экран числа 5, 10 и 21 одно под другим.
+
 package Chapter_1;
 
 public class task_004 {
+
+    public static void main(String[] args) {
+        System.out.println(5);
+        System.out.println(10);
+        System.out.println(21);
+    }
+
 }
