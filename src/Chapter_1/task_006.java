f78dafeb7ec2d5ca85f148b5b6ce07fde0d20d7a
ilya <opt.tzk@gmail.com>
2020-10-15 00:32:20 +0300

    task 6

--- src/Chapter_1/task_006.java
+++ src/Chapter_1/task_006.java
@@ -1,4 +1,14 @@
+//        Число π примерно равно 3,1415926. Вывести на экран это число с тремя цифрами в дробной части.
+//        Текст '3.142' не использовать.
+
+
 package Chapter_1;
 
 public class task_006 {
+
+    public static void main(String[] args) {
+
+        double pi = 3.1415926;
+        System.out.format("%.3f", pi);
+    }
 }
