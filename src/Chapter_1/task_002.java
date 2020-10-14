5079c98a9a9a9de889837f442e1f13adb4666d34
ilya <opt.tzk@gmail.com>
2020-10-14 23:30:15 +0300

    2

--- src/Chapter_1/task_002.java
+++ src/Chapter_1/task_002.java
@@ -1,4 +1,17 @@
+//Вывести на одной строке числа 47, 52 и 150 с двумя пробелами между ними. Текст '47 52 150' не использовать.
+
 package Chapter_1;
 
 public class task_002 {
+
+    public static void main(String[] args) {
+
+        int a = 47;
+        int b = 52;
+        int c = 150;
+
+        System.out.print(a + "  ");
+        System.out.print(b + "  ");
+        System.out.print(c);
+    }
 }
