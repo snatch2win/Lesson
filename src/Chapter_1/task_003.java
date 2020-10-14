ba5c3f2799041678589ab83939ef24d1d8ee2ce1
ilya <opt.tzk@gmail.com>
2020-10-14 23:43:57 +0300

    task 3

--- src/Chapter_1/task_003.java
+++ src/Chapter_1/task_003.java
@@ -1,4 +1,14 @@
+//Вывести на экран числа 50 и 10 одно под другим.
+
+
 package Chapter_1;
 
 public class task_003 {
+
+    public static void main(String[] args) {
+
+        System.out.println(50);
+        System.out.println(10);
+
+    }
 }
