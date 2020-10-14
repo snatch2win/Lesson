3b4c387298455ec2a14fbfce2276736734e5e01e
ilya <opt.tzk@gmail.com>
2020-10-15 00:06:15 +0300

    task 5

--- src/Chapter_1/task_005.java
+++ src/Chapter_1/task_005.java
@@ -1,4 +1,17 @@
+//Получить на экране следующее:
+//1
+//2
+
+
+
 package Chapter_1;
 
 public class task_005 {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+    }
+
+
 }
